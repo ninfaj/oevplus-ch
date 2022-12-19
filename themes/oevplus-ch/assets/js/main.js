d303b07d334836210829830a18577f02b3d7b536
Julian Infanger <julian.infanger@gmail.com>
2022-12-19 16:53:38 +0100

    added carousel

--- themes/oevplus-ch/assets/js/main.js
+++ themes/oevplus-ch/assets/js/main.js
@@ -5,3 +5,13 @@ burger.addEventListener('click', function() {
   burger.classList.toggle('is-active');
   target.classList.toggle('is-active');
 });
+
+// sliders
+document.querySelectorAll(".splide").forEach(function(slider){
+    var splide = new Splide("#" + slider.id, {
+        type: "loop",
+        autoplay: true,
+        interval: 5000
+    });
+    splide.mount();
+})
