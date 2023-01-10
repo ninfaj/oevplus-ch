bbf8a10cdf69cddce7ca0f9b7aab731fa64d49c0
Julian Infanger <julian.infanger@gmail.com>
2023-01-10 13:18:48 +0100

    updated content

--- themes/oevplus-ch/assets/js/main.js
+++ themes/oevplus-ch/assets/js/main.js
@@ -1,28 +1,33 @@
+// clean up empty paragraphs
+document.querySelectorAll("p").forEach(function (paragraph) { 
+  if (!paragraph.innerHTML) paragraph.remove();
+})
+
 // Get all "navbar-burger" elements
 var burger = document.querySelector('.navbar-burger');
-burger.addEventListener('click', function() {
+burger.addEventListener('click', function () {
   var target = document.getElementById(burger.dataset.target);
   burger.classList.toggle('is-active');
   target.classList.toggle('is-active');
 });
 
 // sliders
-document.querySelectorAll(".splide").forEach(function(slider){
-    var splide = new Splide("#" + slider.id, {
-        type: "loop",
-        speed: 1000,
-        autoplay: true,
-        interval: 5000,
-        transition: "fade"
-    });
-    splide.mount();
+document.querySelectorAll(".splide").forEach(function (slider) {
+  var splide = new Splide("#" + slider.id, {
+    type: "loop",
+    speed: 1000,
+    autoplay: true,
+    interval: 5000,
+    transition: "fade"
+  });
+  splide.mount();
 });
 
 // tabs
 var firstTab = document.querySelector("a[data-tab]");
 if (firstTab) showTab(firstTab.dataset.tab);
-document.querySelectorAll("a[data-tab]").forEach(function(link){
-  link.addEventListener("click", function(event) {
+document.querySelectorAll("a[data-tab]").forEach(function (link) {
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     showTab(link.dataset.tab);
   })
@@ -35,21 +40,21 @@ function showTab(tabid) {
 }
 
 function hideAllTabs() {
-  document.querySelectorAll("[data-tab]").forEach(function(tab){
+  document.querySelectorAll("[data-tab]").forEach(function (tab) {
     tab.parentElement.classList.remove("is-active");
   });
-  document.querySelectorAll("[data-tabid]").forEach(function(tabcontent){
+  document.querySelectorAll("[data-tabid]").forEach(function (tabcontent) {
     tabcontent.classList.add("is-hidden");
   });
 }
 
 
 // faq
-document.querySelectorAll(".faq a.message-header").forEach(function(link){
-  link.addEventListener("click", function(event) {
+document.querySelectorAll(".faq a.message-header").forEach(function (link) {
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     var isActive = link.parentElement.classList.contains("is-active");
-    document.querySelectorAll(".faq article").forEach(function(faqItem){
+    document.querySelectorAll(".faq article").forEach(function (faqItem) {
       faqItem.classList.remove("is-active");
     });
     if (!isActive) link.parentElement.classList.add("is-active");
